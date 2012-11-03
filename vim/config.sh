582f66bc9b66056ca7643b78ee7667f817604e8a
seb vincent <seb.vincent@gmail.com>
2012-11-03 13:09:04 +0800

    Update vim/config.sh

--- vim/config.sh
+++ vim/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# install janus first
+# install janus first git://github.com/carlhuda/janus.git
 cp snipmate/html.snippets ~/.vim/janus/vim/tools/snipmate-snippets/snippets/html.snippets
 cp syntastic/html.vim ~/.vim/janus/vim/tools/syntastic/syntax_checkers/html.vim
 
