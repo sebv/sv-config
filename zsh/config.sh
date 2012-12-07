d1c0cfef7ed0f149d57ac2ef19d6cbc6eb4aeb39
Sebastien Vincent <seb.vincent@gmail.com>
2012-12-07 15:32:43 +0800

    config update

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # install oh-my-zsh https://github.com/robbyrussell/oh-my-zsh
-cat .zshrc-cat config.sh >> ~/.zshrc
+mkdir -p ~/Library/Application\ Support/zsh/z
+git clone git://github.com/rupa/z.git ~/Library/Application\ Support/zsh/z
+cat .zshrc-cat >> ~/.zshrc
 
