720c1b0cbd9414871990a28a0240ad8a294963a2
Sebastien Vincent <seb.vincent@gmail.com>
2012-12-07 14:49:39 +0800

    config update

--- vim/config.sh
+++ vim/config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# install janus first git://github.com/carlhuda/janus.git
+# install janus git://github.com/carlhuda/janus.git
+# install PeepOpen https://peepcode.com/products/peepopen
 cp snipmate/html.snippets ~/.vim/janus/vim/tools/snipmate-snippets/snippets/html.snippets
 cp syntastic/html.vim ~/.vim/janus/vim/tools/syntastic/syntax_checkers/html.vim
-
+cp syntastic/sass.vim ~/.vim/janus/vim/tools/syntastic/syntax_checkers/sass.vim
+cp syntastic/scss.vim ~/.vim/janus/vim/tools/syntastic/syntax_checkers/scss.vim
+cp vim/.vimrc.before ~
+cp vim/.vimrc.after ~
 
---
+++ zsh/config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# install oh-my-zsh https://github.com/robbyrussell/oh-my-zsh
+cat .zshrc-cat config.sh >> ~/.zshrc
+
